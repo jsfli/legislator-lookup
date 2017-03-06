e330ccbb934ef7f71c8d7445aff6d5a02913ed77
Jeffrey Li <jeffrey.li@cantab.net>
2017-03-06 15:58:09 +0800

    final commit

--- app/routes/results.js
+++ app/routes/results.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
+// import config from '../config/environment';
 
 export default Ember.Route.extend({
+  model: function(params) {
+    // var key = config.myApiKey;
+    var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?&zip=' + params.zip;
+        // var url = 'http://congress.api.sunlightfoundation.com/legislators/locate?apikey=' +key+ '&zip=' +params.zip;
+    return Ember.$.getJSON(url).then(function(responseJSON){
+      return responseJSON.results;
+    });
+  }
 });
